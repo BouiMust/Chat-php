094673d083de75f50c0303f5129922371403ad95
bouimust <moussovic@gmail.com>
2022-12-08 10:16:32 +0100

    Page de connexion

--- index.php
+++ index.php
@@ -1,34 +1,84 @@
 <?php
 require_once './functions/auth2.php';
-// Variable titre qui dépend de la page (à placer avant le composant header)
+require_once './functions/functions_messages.php';
 $title = 'Page d\'accueil';
 // Importe Header
 require './components/header.php';
 ?>
 
 <main class="container">
-  <div class="bg-light p-5 rounded">
-    <?php if (array_key_exists('login-successed', $_GET) && is_logged()) echo '<p class="alert alert-success">Vous êtes connecté.</p>'; ?>
-    <?php if (array_key_exists('registration-successed', $_GET)) echo '<p class="alert alert-success">Inscription réussie. Vous pouvez désormais vous connecter.</p>'; ?>
-    <?php if (array_key_exists('logout-successed', $_GET) && !is_logged()) echo '<p class="alert alert-success">Vous venez de vous déconnecter.</p>'; ?>
-    <h1>Accueil</h1>
-    <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio hic esse, aperiam quasi quia ullam, commodi quis sunt perspiciatis tempore, omnis velit quo veritatis tempora quidem fugiat blanditiis maxime adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia a incidunt amet, saepe ab eligendi aut illum nesciunt excepturi quis quos quasi voluptas aperiam esse iure et doloremque. Ipsum, architecto! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio veritatis nulla error dicta? Optio saepe incidunt illum delectus expedita, nesciunt magnam fugit corporis atque consequuntur eius eaque ipsa dolorem nam.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio hic esse, aperiam quasi quia ullam, commodi quis sunt perspiciatis tempore, omnis velit quo veritatis tempora quidem fugiat blanditiis maxime adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia a incidunt amet, saepe ab eligendi aut illum nesciunt excepturi quis quos quasi voluptas aperiam esse iure et doloremque. Ipsum, architecto! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio veritatis nulla error dicta? Optio saepe incidunt illum delectus expedita, nesciunt magnam fugit corporis atque consequuntur eius eaque ipsa dolorem nam.</p>
-    <a class="btn btn-lg btn-primary" href=# role="button">Valider</a>
-  </div>
+    <div class="row mx-auto bg-light p-3 rounded">
+
+        <div class="col-12 py-1 mx-auto mb-3 text-center text-light rounded h4" style="background:<?= $theme ?>">ESPACE DISCUSSION</div>
+        <?php if (array_key_exists('login-successed', $_GET) && is_logged()) echo '<p class="alert alert-success p-1 fs-5 text-center">Vous êtes connecté.</p>'; ?>
+        <?php if (array_key_exists('registration-successed', $_GET)) echo '<p class="alert alert-success p-1 fs-5 text-center">Inscription réussie. Vous pouvez désormais vous connecter.</p>'; ?>
+        <?php if (array_key_exists('logout-successed', $_GET) && !is_logged()) echo '<p class="alert alert-success p-1 fs-5 text-center">Vous venez de vous déconnecter.</p>'; ?>
+
+        <div class="row mx-auto">
+
+            <!-- BLOC ASIDE -->
+            <div class="col-3 bg-light py-2 rounded border border-1 border-primary shadow position-fixed" style="min-height:260px">
+                <p class="text-center text-white bg-dark py-2 m-0">Nouveau message</p>
+                <div class="my-1">
+                    <form action='' method='post' class='form-inline'>
+                        <div class="d-flex justify-content-between mb-1">
+                            <div>
+                                <!-- PHOTO PROFILE -->
+                                <img class="rounded" src="<?= isset($_SESSION['user']['photo']) && $_SESSION['user']['photo'] !== '' ? $_SESSION['user']['photo'] : './images/profile.png' ?>" alt="photo de profil" width="40px">
+                                <strong><?= is_logged() ? $_SESSION['user']['pseudo'] : 'inconnu' ?></strong>
+                            </div>
+                            <button type="submit" class="btn btn-success border-dark px-4">Envoyer</button>
+                        </div>
+                        <div class="mt-1">
+                            <textarea class="border-dark w-100" name="message" id="message" rows="5" placeholder="Votre message ici..."></textarea>
+                        </div>
+                    </form>
+                </div>
+                <?php if ($error) echo "<div class='alert alert-danger p-1'>$error</div>" ?>
+            </div>
+
+            <!-- BLOC DES MESSAGES -->
+            <div class="col-8 rounded p-2 text-break" style="min-height:385px;position:relative;left:400px;background:<?= $theme ?>">
+
+                <?php if (!$noMessage) : ?>
+                    <?php foreach (array_reverse($messages) as $message) : ?>
+                        <div class="col-11 bg-light mb-2 pb-1 rounded border border-1 border-primary shadow">
+                            <div class="row text-center mx-auto pb-1">
+                                <div class="col-6 d-flex align-items-center">
+                                    <div>Envoyé par <strong><?= $message['pseudo'] ?></strong></div>
+                                </div>
+                                <div class="col-6 d-flex align-items-center justify-content-end">
+                                    <div>le <?= $message['date'] ?> à <?= $message['time'] ?></div>
+                                </div>
+                            </div>
+                            <div class="border-top border-1 border-primary px-2 py-2">
+                                <div class="col text-break" style="line-height:100%"><?= nl2br(htmlentities(str_replace('\n', "\n", $message['text']))) ?></div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                <?php else : ?>
+                    <div class='col-11 text-light h4'><?= $noMessage ?></div>
+                <?php endif ?>
+
+                <!-- TEMPLATE MESSAGE -->
+                <!-- <div class="col-11 bg-light mb-2 rounded border border-1 border-primary shadow">
+                    <div class="row text-center mx-auto pb-1">
+                        <div class="col-6 d-flex align-items-center">
+                            <div>Envoyé par <strong>$pseudo</strong></div>
+                        </div>
+                        <div class="col-6 d-flex align-items-center justify-content-end">
+                            <div>le $dateMessage à $timeMessage</div>
+                        </div>
+                    </div>
+                    <div class="border-top border-1 border-primary px-2 py-2">
+                        <div class="col text-break" style="line-height:100%">$text</div>
+                    </div>
+                </div> -->
+
+            </div>
+        </div>
+    </div>
 </main>
 
 
 <?php require './components/footer.php' ?>
-
-<!-- 
-Ce projet à pour but de me familiariser avec le langage PHP
-- Talk me more : réseau de discussion
-- dans le jeu, rajouter palindrome
-- classer les utilisateurs par ordre alpha
-- operation CRUD user
-- operation CRUD post
-- validation pseudo et mdp coté front et back
-
--->
