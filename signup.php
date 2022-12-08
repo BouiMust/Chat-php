3adacff39d8adeb93f1ecb41f56f01ad07746d4d
bouimust <moussovic@gmail.com>
2022-12-08 10:15:28 +0100

    Page inscription

--- signup.php
+++ signup.php
@@ -10,31 +10,34 @@ require './components/header.php';
 ?>
 
 <main class="container">
-    <div class="col-md-5 p-5 rounded center" style='text-align:center;margin:10px auto;background:#888;color:#FFF;'>
-        <?php if (is_logged()) : ?>
-            <h2 class="alert">Vous êtes déjà inscrits.</h2>
-            <?= is_admin() ? '<div class="alert alert-danger"><strong> COMPTE ADMINISTATEUR </strong></div>' : '' ?>
-            <a href="/functions/functions_logout.php"><button class='btn btn-secondary'>Se déconnecter</button></a>
-        <?php else : ?>
-            <h2>Créer votre compte</h2>
-            <form action='/signup.php?action=register' method='post' class='form-inline'>
-                <div class='col-md-5 form-group' style='margin: 15px auto;'>
-                    <input type='text' name='pseudo' class='form-control' placeholder='Votre pseudo'>
-                </div>
-                <div class='col-md-5 form-group' style='margin: 15px auto;'>
-                    <input type='email' name='email' class='form-control' placeholder='Votre email'>
-                </div>
-                <div class='col-md-5 form-group' style='margin: 15px auto;'>
-                    <input type='password' name='password' class='form-control' placeholder='Votre mdp'>
-                </div>
-                <button type='submit' class='btn' style='background:#444;color:#FFF;'>Valider</button>
-            </form>
-        <?php endif ?>
-        <p>
-            <?php
-            if ($error) echo '<div class="alert alert-danger">' . $error . '</div>';
-            ?>
-        </p>
+    <div class="row mx-auto bg-light p-3 rounded">
+
+        <div class="col-12 py-1 mx-auto mb-3 text-center text-light rounded h4" style="background:<?= $theme ?>">CRÉER UN COMPTE</div>
+        <div class="col-md-5 p-2 rounded center bg-light" style='text-align:center;margin:10px auto;'>
+            <?php if (is_logged()) : ?>
+                <h2 class="alert">Vous êtes déjà inscrits.</h2>
+                <?= is_admin() ? '<div class="alert alert-danger"><strong>COMPTE ADMINISTATEUR</strong></div>' : '' ?>
+                <a href="./functions/functions_logout.php"><button class='btn btn-secondary'>Se déconnecter</button></a>
+            <?php else : ?>
+                <form action='./signup.php?action=register' method='post' class='form-inline'>
+                    <div class='col-md-5 form-group' style='margin: 15px auto;'>
+                        <input type='text' name='pseudo' class='form-control text-center border-dark' placeholder='Votre pseudo'>
+                    </div>
+                    <div class='col-md-5 form-group' style='margin: 15px auto;'>
+                        <input type='email' name='email' class='form-control text-center border-dark' placeholder='Votre email'>
+                    </div>
+                    <div class='col-md-5 form-group' style='margin: 15px auto;'>
+                        <input type='password' name='password' class='form-control text-center border-dark' placeholder='Votre mdp'>
+                    </div>
+                    <button type='submit' class='btn btn-success'>Envoyer</button>
+                </form>
+            <?php endif ?>
+            <p>
+                <?php
+                if ($error) echo '<div class="alert alert-danger">' . $error . '</div>';
+                ?>
+            </p>
+        </div>
     </div>
 </main>
 
