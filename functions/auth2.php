70bf3c2bebb13635446bfe49a35397d651aed37e
bouimust <moussovic@gmail.com>
2022-12-08 11:55:20 +0100

    Fonction authentification

--- functions/auth2.php
+++ functions/auth2.php
@@ -4,21 +4,21 @@
 // Mettre une auth avant delete ici ?
 
 // Démarre une session si aucune session en cours, pour éviter d'ouvrir plusieurs
-session_status() === 2 ? : session_start();
+session_status() === 2 ?: session_start();
 
 // Vérifie si l'user est connecté
-function is_logged():bool {
+function is_logged(): bool
+{
     return !empty($_SESSION['user']);
 }
 
 // Vérifie si l'user connecté est Admin
 // On retreouve les identifiants admin ici
-function is_admin():bool {
+function is_admin(): bool
+{
     if (is_logged()) {
-        $isAdmin = strtolower($_SESSION['user']['pseudo']) === 'admin' && $_SESSION['user']['password'] === '$2y$12$HGe6kHfY1mlugxnynTdnbOL4lcA1mYcs8k/Pa00cGGWIHyBOI.bZ.';
+        $isAdmin = strtolower($_SESSION['user']['pseudo']) === 'admin' && $_SESSION['user']['password'] === '$2y$12$cnqco.0cKmKEjA4u9qQTge6LHav1D7Ug/5JwxL601HOnDlUl22NoO';
         return $isAdmin;
     }
     return false;
 }
-
-?>
