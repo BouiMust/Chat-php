75afa089bafbbcc6ce87578501dc3019f182cb2c
bouimust <moussovic@gmail.com>
2023-02-05 10:37:01 +0100

    Tableau de bord admin

--- dashboard.php
+++ dashboard.php
@@ -28,21 +28,21 @@ $title = 'Tableau de bord';
         <div class="col-12 py-1 mx-auto mb-3 text-center text-light rounded h4" style="background:<?= $theme ?>">TABLEAU DE BORD</div>
         <?php if (array_key_exists('account-deleted', $_GET)) echo '<p class="alert alert-success p-1 fs-4 text-center">Compte utilisateur supprimé.</p>'; ?>
         <?php if (array_key_exists('administrator', $_GET)) echo '<p class="alert alert-danger p-1 fs-4 text-center">Vous ne pouvez pas supprimer votre compte.</p>'; ?>
-        
+
         <!-- BLOC USER -->
         <div class="p-5 rounded row" style="text-align:center;">
-            <h1 style=''> Nombre d'utilisateurs inscrits </h1>
+            <h1>Nombre d'utilisateurs inscrits</h1>
             <h2 class='col-md-5' style='border:1px solid;margin:auto;'><?= count($users) ?></h2>
-            <h1 style=''> Liste des utilisateurs </h1>
+            <h1>Liste des utilisateurs</h1>
             <table class='col-md-9' style='margin:auto;border:1px solid'>
-            <tr style='border:1px solid;'>
-                <th>IDENTIFIANT</th>
-                <th>PSEUDO</th> <!-- header -->
-                <th>EMAIL</th>
-                <th>PASSWORD</th>
-                <th></th>
-            </tr>
-            <?= show_users($users) ?>
+                <tr style='border:1px solid;'>
+                    <th>IDENTIFIANT</th>
+                    <th>PSEUDO</th> <!-- header -->
+                    <th>EMAIL</th>
+                    <th>PASSWORD</th>
+                    <th></th>
+                </tr>
+                <?= show_users($users) ?>
             </table>
         </div>
 
@@ -53,22 +53,26 @@ $title = 'Tableau de bord';
                     <span style='font-weight:bold;color:#58A;'>Séléctionner une année</span>
                     <?php foreach (YEARS as $year) : ?>
                         <?php $isSelected = $year === (int)$yearSelected ?>
-                        <div class="radio" <?php if ($isSelected) : echo 'style="background:#ADF;"'; endif ?>>
+                        <div class="radio" <?php if ($isSelected) : echo 'style="background:#ADF;"';
+                                            endif ?>>
                             <label>
-                                <input type='radio' name='year' value=<?=$year?> <?php if ($isSelected) : echo 'checked'; endif ?>>
-                                <?=$year?>
+                                <input type='radio' name='year' value=<?= $year ?> <?php if ($isSelected) : echo 'checked';
+                                                                                    endif ?>>
+                                <?= $year ?>
                             </label>
                         </div>
                     <?php endforeach ?>
                 </div>
                 <div class='form-group' style='margin:10px;padding:5px;background:#EEE;'>
                     <span style='font-weight:bold;color:#58A;'>Séléctionner un mois</span>
-                    <?php foreach (MONTHS as $month) : ?> 
+                    <?php foreach (MONTHS as $month) : ?>
                         <?php $isSelected = $month === $monthSelected ?>
-                        <div class="radio" <?php if ($isSelected) : echo 'style="background:#ADF;"'; endif ?>>
+                        <div class="radio" <?php if ($isSelected) : echo 'style="background:#ADF;"';
+                                            endif ?>>
                             <label>
-                                <input type='radio' name='month' value=<?=$month?> <?php if ($isSelected) : echo 'checked'; endif ?>>
-                                <?=$month?>
+                                <input type='radio' name='month' value=<?= $month ?> <?php if ($isSelected) : echo 'checked';
+                                                                                        endif ?>>
+                                <?= $month ?>
                             </label>
                         </div>
                     <?php endforeach ?>
@@ -76,26 +80,26 @@ $title = 'Tableau de bord';
                 <button type='submit' class='btn btn-primary col-md-12'>Valider</button>
             </form>
         </div>
-        <div class='col-md-8'  style='color:#479;'>
+        <div class='col-md-8' style='color:#479;'>
             <div>
-                <h2 >Nombre de vues aujourd'hui (<?= date('d/m/Y') ?>)</h2>
+                <h2>Nombre de vues aujourd'hui (<?= date('d/m/Y') ?>)</h2>
                 <p style='font-size:2em;'><strong> <?= show_view($todayView) ?></strong></p>
             </div>
             <div>
-                <h2 >Nombre de vues en <?= $monthSelected . ' ' . $yearSelected ?></h2>
+                <h2>Nombre de vues en <?= $monthSelected . ' ' . $yearSelected ?></h2>
                 <p style='font-size:2em;'><strong> <?= show_view($timeSelectedView) ?></strong></p>
             </div>
             <div>
-                <h2 >Nombre de vues en <?= $yearSelected ?></h2>
+                <h2>Nombre de vues en <?= $yearSelected ?></h2>
                 <p style='font-size:2em;'><strong> <?= show_view($yearSelectedView) ?></strong></p>
             </div>
             <div>
-                <h2 >Moyenne de vues en <?= $yearSelected ?> (par jour)</h2>
+                <h2>Moyenne de vues en <?= $yearSelected ?> (par jour)</h2>
                 <p style='font-size:2em;'><strong> <?= round(show_view($yearSelectedView) / 365, 2) ?></strong></p>
             </div>
         </div>
     </div>
-    
+
 
 </main>
 
