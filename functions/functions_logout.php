d46b86093f3a9a4795ac678e029da1c36f62092f
bouimust <moussovic@gmail.com>
2023-02-05 10:34:12 +0100

    Fonction de déconnexion

--- functions/functions_logout.php
+++ functions/functions_logout.php
@@ -1,9 +1,12 @@
 <?php
 // LOGOUT : deconnecte l'user quand il clique sur logout
 
-// require_once './auth2.php';
+// démarre la session pour accéder aux datas de session
 session_start();
+// supprime les données de session
 unset($_SESSION['user']);
+// on peut aussi utiliser session_destroy(), ça détruit toutes les données associées à la session courante
+// session_destroy();
 $success_msg = 'Déconnecté !';
 header('Location: ../index.php?logout-successed');
 exit();
